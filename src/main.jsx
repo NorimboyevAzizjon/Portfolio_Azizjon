4444ab24c28a0ef4b985d5c7f72a9c90aa36a293
Azizjon | Frontend Dev <norimboyevazizjon29@gmail.com>
2026-01-07 20:56:41 +0500

    Update dependencies, modify main files, and remove unused logo

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
-import { HelmetProvider } from 'react-helmet-async';
+
 import './index.css';
 
 // Font Awesome
@@ -41,10 +41,8 @@ document.head.appendChild(favicon);
 // Render app
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </HelmetProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
